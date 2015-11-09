efec4e9b6ece53f2a74d2d53160f7e7ae9e1e392
Luca Zanrosso <lucazanrosso94@gmail.com>
2015-11-09 20:23:28 +0100

    Finish to build Moto, Polizza and TestMotoPolizza and complete exercise n. 3

--- Esercitazione3/Polizza.java
+++ Esercitazione3/Polizza.java
@@ -1,16 +1,29 @@
 public class Polizza {
 
+	/*
+	3.2.8
+	Variabili d'istanza.
+	*/
 	private Moto moto;
 	private int anniDurata;
 	private int annoStipula;
 	private int bonusMalus = 1000;
 	
+	/*
+	3.2.9
+	Costruttore che prende in input un oggetto Moto, gli anni di durata della polizza e 
+	l'anno di stipula.
+	*/
 	public Polizza(Moto moto, int anniDurata, int annoStipula) {
 		this.moto = moto;
 		this.anniDurata = anniDurata;
 		this.annoStipula = annoStipula;
 	}
 	
+	/*
+	3.2.10
+	Metodi getter.
+	*/
 	public Moto getMoto() {
 		return this.moto;
 	}
@@ -27,11 +40,26 @@ public class Polizza {
 		return bonusMalus;
 	}
 	
+	/*
+	3.2.11
+	Metodo statico aggiornaBonusMalus.
+	*/
 	public static void aggiornaBonusMalus(Polizza polizza, int bonusMalus) {
 		polizza.bonusMalus += bonusMalus;
 	}
 	
-	// Non è il massimo usare il metodo toString per visualizzare cosi tanti dati
+	/*
+	Questo è il metodo non statico alternativo al metodo statico aggiornaBonusMalus. 
+	public void aggiornaBonusMalus(int bonusMalus) {
+		this.bonusMalus += bonusMalus;
+	}
+	*/
+	
+	/*
+	3.2.12
+	Sovrascrivo il metodo toString. Vale lo stesso discorso fatto per il metodo equals della
+	classe Moto.
+	*/
 	public String toString() {
 		return "Proprietario: " + moto.getProprietario() + "\n" +
 				"Targa: " + moto.getTarga() + "\n" +
@@ -40,5 +68,4 @@ public class Polizza {
 				"Anno di stipula della Polizza: " + this.getAnnoStipula() + "\n" + 
 				"Bonus Malus: " + this.getBonusMalus() + "\n";
 	}
-
 }
