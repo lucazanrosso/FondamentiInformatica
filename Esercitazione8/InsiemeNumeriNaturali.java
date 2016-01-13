0819aad34c0573e6dcf0a5957277d3a5b7f9b7f0
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-01-13 11:20:09 +0100

    Add union method to InsiemeNumeriNaturali class

--- Esercitazione8/InsiemeNumeriNaturali.java
+++ Esercitazione8/InsiemeNumeriNaturali.java
@@ -1,28 +1,91 @@
 public class InsiemeNumeriNaturali {
 	
-	boolean[] numeriNaturali;
+	private boolean[] insieme;
+	
+	public InsiemeNumeriNaturali() {
+		this.insieme = new boolean[0];
+	}
 	
 	public InsiemeNumeriNaturali(int dimensione) {
-		numeriNaturali = new boolean[dimensione];
+		this.insieme = new boolean[dimensione];
 	}
 	
-	public void inserisci(int numero) {
-		numeriNaturali[numero] = true;
+	public boolean[] get() {
+		return this.insieme;
 	}
 	
-	public void cancella(int numero) {
-		numeriNaturali[numero] = false;
+	public void set(boolean[] insieme) {
+		this.insieme = insieme;
+	}
+	
+	public int inserisci(int numero) {
+		this.insieme[numero] = true;
+		return numero;
+	}
+	
+	public int cancella(int numero) {
+		this.insieme[numero] = false;
+		return numero;
 	}
 	
 	public boolean controllaVuoto() {
-		for (boolean posizione : numeriNaturali) {
-			if (posizione == true)
+		for (boolean posizione : this.insieme) {
+			if (posizione)
 				return false;
 		}
 		return true;
 	}
 	
 	public boolean appartiene(int numero) {
-		return numeriNaturali[numero];
+		return this.insieme[numero];
+	}
+	
+	public InsiemeNumeriNaturali unione(InsiemeNumeriNaturali insieme2) {
+		InsiemeNumeriNaturali insiemeUnione = new InsiemeNumeriNaturali();
+		boolean[] i1 = this.insieme;
+		boolean[] i2 = insieme2.get();
+		if (i2.length > i1.length) {
+			boolean[] temp = i1;
+			i1 = i2;
+			i2 = temp;
+		}		
+		insiemeUnione.set(i1);
+		for (int i = 0; i < i2.length; i++) {
+			if (i2[i])
+				insiemeUnione.inserisci(i);
+		}
+		return insiemeUnione;
+	}
+	
+	/*public void intersezione(int[] insieme2) {
+		boolean[] insieme2 = insiemeUnione.get();
+		if (insieme2.length < this.insieme.length) {
+			boolean[] temp = this.insieme;
+			this.insieme = insieme2;
+			insieme2 = temp;
+		}			
+		for (int i = 0; i < insieme2.length; i++) {
+			if (insieme[i] || insieme2[i])
+				this.insieme[i] = false;
+		}
+	}*/
+	
+	public void complemento(int[] insieme2) {
+		
+	}
+	
+	public String stampa() {	
+		String elementi = null;
+		boolean primoElemento = true;
+		for (int i = 0; i < insieme.length; i++) {
+			if (insieme[i]) {
+				if(primoElemento) {
+					elementi = String.valueOf(i);
+					primoElemento = false;
+				} else
+					elementi += (", " + i);
+			}
+		}
+		return elementi;
 	}
 }
--- Esercitazione8/InsiemeNumeriNaturaliTest.java
+++ Esercitazione8/InsiemeNumeriNaturaliTest.java
@@ -2,25 +2,40 @@ public class InsiemeNumeriNaturaliTest {
 	
 	public static void main(String[] args) {
 		
-		InsiemeNumeriNaturali n1 = new InsiemeNumeriNaturali(10);
+		InsiemeNumeriNaturali i1 = new InsiemeNumeriNaturali(6);
 		
-		n1.inserisci(1);
-		n1.inserisci(3);
+		System.out.println("Inserito numero " + i1.inserisci(1) + " a i1");
+		System.out.println("Inserito numero " + i1.inserisci(3) + " a i1");
+		System.out.println("Inserito numero " + i1.inserisci(4) + " a i1");
 		
-		n1.cancella(3);
+		System.out.println("Cancellato numero " + i1.cancella(3) + " a i1");
 		
-		if (n1.controllaVuoto())
-			System.out.println("L'insieme \u00E8 vuoto");
+		if (i1.controllaVuoto())
+			System.out.println("L'insieme i1 \u00E8 vuoto");
 		else
-			System.out.println("L'insieme non \u00E8 vuoto");
+			System.out.println("L'insieme i1 non \u00E8 vuoto");
 		
-		if (n1.appartiene(1))
-			System.out.println("1 appartiene all'insieme");
+		if (i1.appartiene(1))
+			System.out.println("1 appartiene all'insieme i1");
 		else
-			System.out.println("1 non appartiene all'insieme");		
-		if (n1.appartiene(3))
-			System.out.println("3 appartiene all'insieme");
+			System.out.println("1 non appartiene all'insieme i1");		
+		if (i1.appartiene(3))
+			System.out.println("3 appartiene all'insieme i1");
 		else
-			System.out.println("3 non appartiene all'insieme");
+			System.out.println("3 non appartiene all'insieme i1");
+		
+		InsiemeNumeriNaturali i2 = new InsiemeNumeriNaturali(10);
+		System.out.println("Inserito numero " + i2.inserisci(3) + " a i2");
+		System.out.println("Inserito numero " + i2.inserisci(4) + " a i2");
+		System.out.println("Inserito numero " + i2.inserisci(6) + " a i2");
+		System.out.println("Inserito numero " + i2.inserisci(7) + " a i2");
+		System.out.println("Inserito numero " + i2.inserisci(9) + " a i2");
+		
+		System.out.println("i1: " + i1.stampa());
+		System.out.println("i2: " + i2.stampa());
+		InsiemeNumeriNaturali i3 = i1.unione(i2);
+		System.out.println("i1 unito a i3: " + i3.stampa());
+		i1.unione(i2);
+		System.out.println("i1 unito a i3: " + i3.stampa());
 	}
 }
